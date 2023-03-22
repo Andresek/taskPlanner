0e38f02053542e0f71d11fb68a135e299d9bffc5
Andrew S <samum_94@mail.ru>
2023-03-22 22:19:50 +0300

    taskplanner created

---
+++ src/test/java/TasksTest.java
@@ -0,0 +1,64 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TasksTest {
+
+    @Test
+    public void matchesSimpleTask() {
+        SimpleTask simpleTask = new SimpleTask(45, "Стирка");
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+
+        boolean expected  = true;
+        boolean actual = simpleTask.matches("Стирка");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void matchesEpic() {
+
+        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        Epic epic = new Epic(15, subtasks);
+        Todos todos = new Todos();
+
+        todos.add(epic);
+
+        boolean expected  = true;
+        boolean actual = epic.matches("Хлеб");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void matchesMeetingContainsTopic() {
+
+        Meeting meeting = new Meeting(32, "Сбор урожая", "Светлый путь", "В 06:00");
+        Todos todos = new Todos();
+
+        todos.add(meeting);
+
+        boolean expected  = true;
+        boolean actual = meeting.matches("Сбор урожая");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void matchesMeetingContainsProject() {
+
+        Meeting meeting = new Meeting(32, "Сбор урожая", "Светлый путь", "В 06:00");
+        Todos todos = new Todos();
+
+        todos.add(meeting);
+
+        boolean expected  = true;
+        boolean actual = meeting.matches("Светлый путь");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+
+}
---
+++ src/test/java/TodosTest.java
@@ -0,0 +1,92 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class TodosTest {
+
+    @Test
+    public void shouldAddThreeTasksOfDifferentType() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {simpleTask, epic, meeting};
+        Task[] actual = todos.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
+    public void search1() {
+
+        SimpleTask task1 = new SimpleTask(1, "завтрак");
+        SimpleTask task2 = new SimpleTask(2, "обед");
+        SimpleTask task3 = new SimpleTask(3, "ужин");
+
+        Todos todos = new Todos();
+        todos.add(task2);
+        todos.add(task1);
+        todos.add(task3);
+
+        Task[] expected = {task1};
+        Task[] actual = todos.search("завтрак");
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void search2() {
+
+        SimpleTask task1 = new SimpleTask(1, "завтрак");
+        SimpleTask task2 = new SimpleTask(2, "обед");
+        SimpleTask task3 = new SimpleTask(3, "обед");
+
+        Todos todos = new Todos();
+        todos.add(task2);
+        todos.add(task1);
+        todos.add(task3);
+
+        Task[] expected = {task2, task3};
+        Task[] actual = todos.search("обед");
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void search3() {
+
+        SimpleTask task1 = new SimpleTask(1, "завтрак");
+        SimpleTask task2 = new SimpleTask(2, "обед");
+        SimpleTask task3 = new SimpleTask(3, "обед");
+
+        Todos todos = new Todos();
+        todos.add(task2);
+        todos.add(task1);
+        todos.add(task3);
+
+        Task[] expected = {};
+        Task[] actual = todos.search("ужин");
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+}
+
+
+
